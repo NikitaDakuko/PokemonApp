9f4ffb3f1c908c5b7875c0757a65bdf734c86d6b
NikitaDakuko <nikita.dakuko@gmail.com>
2024-04-03 19:50:10 +0300

    Lazy image loading + custom launch icon

--- lib/View/pokemon_view.dart
+++ lib/View/pokemon_view.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:pokemon_application/Entity/enum_poke_type.dart';
 import '../Entity/pokemon.dart';
@@ -15,52 +16,64 @@ class PokemonView extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(pokemon.name),
       ),
-      body: Wrap(
-        children: [
-          Image.network(
-            pokemon.pictureURL,
-            scale: 0.3,
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Name: ${pokemon.name}',
-                style: ts,
+      body: Center(
+        child: Wrap(
+          children: [
+            SizedBox(
+              height: 240,
+              width: 240,
+              child: CachedNetworkImage(
+                fit: BoxFit.fill,
+                imageUrl: pokemon.pictureURL,
+                progressIndicatorBuilder: (context, url, downloadProgress) =>
+                    CircularProgressIndicator(value: downloadProgress.progress),
+                errorWidget: (context, url, error) => const Icon(
+                  Icons.error_outline,
+                  size: 75,
+                ),
               ),
-              Text(
-                'Weight: ${pokemon.weight}',
-                style: ts,
-              ),
-              Text(
-                'Height: ${pokemon.height}',
-                style: ts,
-              ),
-              Wrap(
-                children: [
-                  for (PokeType t in pokemon.types)
-                    Container(
-                      margin: const EdgeInsets.all(5.0),
-                      padding: const EdgeInsets.all(5.0),
-                      decoration: BoxDecoration(
-                        color: t.color,
-                        borderRadius: BorderRadius.circular(10.0),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Name: ${pokemon.name}',
+                  style: ts,
+                ),
+                Text(
+                  'Weight: ${pokemon.weight}',
+                  style: ts,
+                ),
+                Text(
+                  'Height: ${pokemon.height}',
+                  style: ts,
+                ),
+                Wrap(
+                  children: [
+                    for (PokeType t in pokemon.types)
+                      Container(
+                        margin: const EdgeInsets.all(5.0),
+                        padding: const EdgeInsets.all(5.0),
+                        decoration: BoxDecoration(
+                          color: t.color,
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        child: Text(
+                          t.name,
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w200,
+                              color: t.color.computeLuminance() < 0.5
+                                  ? Colors.white
+                                  : Colors.black),
+                        ),
                       ),
-                      child: Text(
-                        t.name,
-                        style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.w200,
-                            color: t.color.computeLuminance() < 0.5
-                                ? Colors.white
-                                : Colors.black),
-                      ),
-                    ),
-                ],
-              )
-            ],
-          ),
-        ],
+                  ],
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
